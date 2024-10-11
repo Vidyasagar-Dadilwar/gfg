be4ba7ef4730d4434b84c1615b6a6e38d7f262aa
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-10-11 20:00:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.cpp
@@ -0,0 +1,13 @@
+...// } Driver Code Ends
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution {
+  public:
+    int peakElement(int arr[], int n)
+    {
+       
+        if (n == 1)
+            return 0;
