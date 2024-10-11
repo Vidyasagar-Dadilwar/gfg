78522e57c0b7aad358f84caa9874c1739228def2
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-10-11 09:06:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest Pair Sum/largest-pair-sum.cpp
+++ Difficulty: Easy/Largest Pair Sum/largest-pair-sum.cpp
@@ -1,1 +1,12 @@
-undefined
+null...// } Driver Code Ends
+
+class Solution {
+public:
+int pairsum(vector<int> &arr) {
+// code here
+int n1 = *max_element(arr.begin(), arr.end());
+int n2;
+for(auto i: arr){
+if(i>n2 && i<n1){
+n2 = i;
+}
