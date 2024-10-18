d280ddb92ec32fe4e6e1a6289269b3022b4d74a0
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-10-19 01:08:41 +0530

    Time: 42 ms (5.45%), Space: 9180000 (69.82%) - LeetHub

---
+++ Easy/2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int countValidWords(string sentence) {
+        int count = 0;
+        istringstream iss(sentence);
+        string word; 
+        
+        while (iss >> word) {
+            int n = word.size();
+            bool pun = false, hyp = false, digi = false, check = true;
+            cout<<word<<endl;
+            for (int i = 0; i < n; i++) {
+                if (word[i] > 47 && word[i] < 58) {
+                    check = false;
+                    break;
+                }
+                else if (pun || ((word[i] == '.' || word[i] == '!' || word[i] == ',') && i != n - 1)) {
+                    check = false;
+                    break;
+                }
+                else if (word[i] == '-' && (hyp || i == 0 || i == n - 1 || (i < n - 1 && (word[i + 1] < 'a' || word[i + 1] > 'z')))) {
+                    check = false;
+                    break;
+                }
+                else {
+                    if (word[i] == '-') 
+                        hyp = true;
+                    else if (word[i] == '.' || word[i] == '!' || word[i] == ',') 
+                        pun = true;
+                }
+            }
+
+            if (check){
+                count++;
+            }
+        }
+        return count;
+    }
+};
