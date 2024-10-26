449019a9b3d83fa5ca1a5de5eb99f38d936309bd
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-10-26 22:59:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two sum -Pairs with 0 Sum/two-sum-pairs-with-0-sum.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> getPairs(vector<int>& arr) {
+        // code here
+        unordered_map<int, int> m ;
+        sort(arr.begin(),arr.end());
+        for(auto x : arr){
+            m[x]++;
+        }
+        vector<vector<int>> ans;
+        for(auto x : arr){
+            int r=-1*x;
+            if(m[r]>=1&&m[x]>=1&&x!=0){
+                vector<int> temp;
+                temp.push_back(x);
+                temp.push_back(r);
+                ans.push_back(temp);
+                m[r]=-1;
+                m[x]=-1;
+            }
+        }
+        if(m[0]>=2){
+            vector<int> temp;
+            temp.push_back(0);
+            temp.push_back(0);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+//Back-end complete function Template for C++
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        // char c;cin>>c;
+        Solution obj;
+        vector<vector<int>> res = obj.getPairs(arr);
+        if (res.size() == 0) {
+            cout << endl;
+        } else
+            Matrix::print(res);
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
