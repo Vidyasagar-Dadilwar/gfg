1a9a4521077c5b6e012967aa992d3a6dcdf0dc4c
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-12-24 15:37:44 +0530

    Added solution CodeChef to GitHub

---
+++ Codechef/SPC2025/spc2025.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n, m;
+    cin>>n>>m;
+    if(n/2 <= m){
+        cout<<"NEWBIE\n";
+    }
+    else{
+        cout<<"PRO\n";
+    }
+}
