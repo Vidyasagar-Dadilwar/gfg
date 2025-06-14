104ed5e7ec9c39c76dcb10680391327ed3d4adca
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-06-14 16:22:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tribonacci Number/tribonacci-number.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    int tribo(int n, vector<int>& dp){
+        if(n == 0)
+            return 0;
+        if(n == 1 || n == 2)
+            return 1;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = tribo(n-3, dp) + tribo(n-1, dp) + tribo(n-2, dp);
+    }
+    int nthTribonacci(int n) {
+        // code here
+        vector<int> dp(n+1, -1);
+        return tribo(n, dp);
+    }
+};
